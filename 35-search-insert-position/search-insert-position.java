e93eae72927aacd6a527a718845ca4f3261d9a2d
Pragati Yadav <pragati.yadav.cs27@iilm.edu>
2026-09-04 21:19:22 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (83.35%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left=0;
+        int right=nums.length-1;
+        int mid;
+        while(left<=right){
+            mid=left+(right-left)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]<target){
+                left=1+mid;
+            }
+            else{
+                right=mid-1;
+            }
+        }
+        return left;
+    }
+}
